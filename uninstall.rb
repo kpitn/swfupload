094a235e3ca620abba412e636e35fa6519a5f1a5
pierre <pierre@Stormwind.(none)>
2009-01-25 14:14:51 +0100

    fix desinstallation

--- uninstall.rb
+++ uninstall.rb
@@ -1,7 +1,7 @@
 # Uninstall hook code here
 puts "Removing files..."
 dir = "/javascripts/swf/"
-["swfupload.js", "swfupload.swf", "swfupload.cookies.js","fileprogress.js","handlers.js","swfupload.cookie.js"].each do |js_file|
+["swfupload.js", "swfupload.swf", "swfupload.cookies.js","fileprogress.js","handlers.js"].each do |js_file|
 	File.delete(RAILS_ROOT+"/public"+dir+ js_file)
 end
 puts "Files removed - Desinstallation complete!"
