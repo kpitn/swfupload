7deb9f0e5b19757561200df3cf4b9657643d9e38
pierre <pierre@Stormwind.(none)>
2009-03-21 17:51:29 +0100

    add support for rails 2.3.0 and authenticity_token with middleware

--- lib/swfupload.rb
+++ lib/swfupload.rb
@@ -1,27 +1,18 @@
-class CGI::Session
-  alias original_initialize initialize
-  def initialize(request, option = {})
-    session_key = option['session_key'] || '_session_id'
-    query_string = if (qs = request.env_table["QUERY_STRING"]) and qs != ""
-      qs
-    elsif (ru = request.env_table["REQUEST_URI"][0..-1]).include?("?")
-      ru[(ru.index("?") + 1)..-1]
+require 'rack/utils'
+
+class FlashSessionCookieMiddleware
+  def initialize(app, session_key = '_session_id')
+    @app = app
+    @session_key = session_key
+  end
+
+  def call(env)
+    if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
+      params = ::Rack::Utils.parse_query(env['QUERY_STRING'])
+      env['HTTP_COOKIE'] = [ @session_key, params[@session_key] ].join('=').freeze unless params[@session_key].nil?
     end
-    if query_string and query_string.include?(session_key)
-      option['session_data'] = CGI.unescape(query_string.scan(/#{session_key}=(.*?)(&.*?)*$/).flatten.first)
-    end
-    original_initialize(request, option)
+    @app.call(env)
   end
 end
 
-class CGI::Session::CookieStore
-  alias original_initialize initialize
-  def initialize(session, options = {})
-    @session_data = options['session_data']
-    original_initialize(session, options)
-  end
-
-  def read_cookie
-    @session_data || @session.cgi.cookies[@cookie_options['name']].first
-  end
-end
+ActionController::Dispatcher.middleware.use FlashSessionCookieMiddleware, ActionController::Base.session_options[:key]
